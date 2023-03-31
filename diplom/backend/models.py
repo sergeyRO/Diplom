f8563f29776697363d8128b7a0b8c93bba257a40
SergeyRO <sergey_r.o@mail.ru>
2023-03-31 21:06:52 +0300

    first

--- diplom/backend/models.py
+++ diplom/backend/models.py
@@ -60,43 +60,43 @@ USER_TYPE_CHOICES = (
 #         return self._create_user(email, password, **extra_fields)
 #
 #
-# class User(AbstractUser):
-#     """
-#     Стандартная модель пользователей
-#     """
-#     REQUIRED_FIELDS = []
-#     objects = UserManager()
-#     USERNAME_FIELD = 'email'
-#     email = models.EmailField(_('email address'), unique=True)
-#     company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
-#     position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
-#     username_validator = UnicodeUsernameValidator()
-#     username = models.CharField(
-#         _('username'),
-#         max_length=150,
-#         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-#         validators=[username_validator],
-#         error_messages={
-#             'unique': _("A user with that username already exists."),
-#         },
-#     )
-#     is_active = models.BooleanField(
-#         _('active'),
-#         default=False,
-#         help_text=_(
-#             'Designates whether this user should be treated as active. '
-#             'Unselect this instead of deleting accounts.'
-#         ),
-#     )
-#     type = models.CharField(verbose_name='Тип пользователя', choices=USER_TYPE_CHOICES, max_length=5, default='buyer')
-#
-#     def __str__(self):
-#         return f'{self.first_name} {self.last_name}'
-#
-#     class Meta:
-#         verbose_name = 'Пользователь'
-#         verbose_name_plural = "Список пользователей"
-#         ordering = ('email',)
+class User(AbstractUser):
+    """
+    Стандартная модель пользователей
+    """
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(_('email address'), unique=True)
+    company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
+    position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=False,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    type = models.CharField(verbose_name='Тип пользователя', choices=USER_TYPE_CHOICES, max_length=5, default='buyer')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = "Список пользователей"
+        ordering = ('email',)
 
 
 class Shop(models.Model):
