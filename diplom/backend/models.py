2570db0e6cb730a3f1df98744ea5ac55c8f4c1c8
SergeyRO <sergey_r.o@mail.ru>
2023-03-31 21:02:06 +0300

    first

--- diplom/backend/models.py
+++ diplom/backend/models.py
@@ -25,88 +25,78 @@ USER_TYPE_CHOICES = (
 # Create your models here.
 
 
-class UserManager(BaseUserManager):
-    """
-    Миксин для управления пользователями
-    """
-    use_in_migrations = True
-
-    # def _create_user(self, email, password, **extra_fields):
-    #     """
-    #     Create and save a user with the given username, email, and password.
-    #     """
-    #     if not email:
-    #         raise ValueError(_('The given email must be set'))
-    #     email = self.normalize_email(email)
-    #     user = self.model(email=email, **extra_fields)
-    #     user.set_password(password)
-    #     user.save(using=self._db)
-    #     return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        #return self._create_user(email, password, **extra_fields)
-        """
-                Create and save a user with the given username, email, and password.
-                """
-        if not email:
-            raise ValueError(_('The given email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
-
-
-class User(AbstractUser):
-    """
-    Стандартная модель пользователей
-    """
-    REQUIRED_FIELDS = []
-    objects = UserManager()
-    USERNAME_FIELD = 'email'
-    email = models.EmailField(_('email address'), unique=True)
-    company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
-    position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
-    username_validator = UnicodeUsernameValidator()
-    username = models.CharField(
-        _('username'),
-        max_length=150,
-        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
-    )
-    is_active = models.BooleanField(
-        _('active'),
-        default=False,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-    type = models.CharField(verbose_name='Тип пользователя', choices=USER_TYPE_CHOICES, max_length=5, default='buyer')
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = "Список пользователей"
-        ordering = ('email',)
+# class UserManager(BaseUserManager):
+#     """
+#     Миксин для управления пользователями
+#     """
+#     use_in_migrations = True
+#
+#     def _create_user(self, email, password, **extra_fields):
+#         """
+#         Create and save a user with the given username, email, and password.
+#         """
+#         if not email:
+#             raise ValueError(_('The given email must be set'))
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_user(self, email, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', False)
+#         extra_fields.setdefault('is_superuser', False)
+#         return self._create_user(email, password, **extra_fields)
+#
+#     def create_superuser(self, email, password, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
+#
+#         if extra_fields.get('is_staff') is not True:
+#             raise ValueError('Superuser must have is_staff=True.')
+#         if extra_fields.get('is_superuser') is not True:
+#             raise ValueError('Superuser must have is_superuser=True.')
+#
+#         return self._create_user(email, password, **extra_fields)
+#
+#
+# class User(AbstractUser):
+#     """
+#     Стандартная модель пользователей
+#     """
+#     REQUIRED_FIELDS = []
+#     objects = UserManager()
+#     USERNAME_FIELD = 'email'
+#     email = models.EmailField(_('email address'), unique=True)
+#     company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
+#     position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
+#     username_validator = UnicodeUsernameValidator()
+#     username = models.CharField(
+#         _('username'),
+#         max_length=150,
+#         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+#         validators=[username_validator],
+#         error_messages={
+#             'unique': _("A user with that username already exists."),
+#         },
+#     )
+#     is_active = models.BooleanField(
+#         _('active'),
+#         default=False,
+#         help_text=_(
+#             'Designates whether this user should be treated as active. '
+#             'Unselect this instead of deleting accounts.'
+#         ),
+#     )
+#     type = models.CharField(verbose_name='Тип пользователя', choices=USER_TYPE_CHOICES, max_length=5, default='buyer')
+#
+#     def __str__(self):
+#         return f'{self.first_name} {self.last_name}'
+#
+#     class Meta:
+#         verbose_name = 'Пользователь'
+#         verbose_name_plural = "Список пользователей"
+#         ordering = ('email',)
 
 
 class Shop(models.Model):
