b16622fffef75d8bc4ea9e3e62ff53eaa2354105
SergeyRO <sergey_r.o@mail.ru>
2023-04-01 19:38:54 +0300

    first

--- diplom/netology_pd_diplom/settings.py
+++ diplom/netology_pd_diplom/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=hs6$#5om031nujz4staql9mbuste=!dc^6)4opsjq!vvjxzj@'
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = '=hs6$#5om031nujz4staql9mbuste=!dc^6)4opsjq!vvjxzj@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
