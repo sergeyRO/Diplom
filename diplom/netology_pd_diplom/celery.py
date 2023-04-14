49f0b404e528761cc225b10534cfcf6eb28bd8b6
SergeyRO <sergey_r.o@mail.ru>
2023-04-14 13:30:35 +0300

    second

--- diplom/netology_pd_diplom/celery.py
+++ diplom/netology_pd_diplom/celery.py
@@ -1,10 +1,6 @@
 import os
 
 from celery import Celery
-from backend.views import yaml_in_db
-from backend.signals import password_reset_token_created,\
-    new_user_registered_signal, new_order_signal,\
-    new_order_admin_signal, new_order_contact_signal
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netology_pd_diplom.settings")
 
