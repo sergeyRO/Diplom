dc470886782cb3fbd41690098be6a887ce1fcf85
SergeyRO <sergey_r.o@mail.ru>
2023-04-15 10:09:40 +0300

    second

--- diplom/backend/signals.py
+++ diplom/backend/signals.py
@@ -46,14 +46,15 @@ new_order_contact = Signal('user_id')
 
 @app.task
 def send_message(user_id):
-    token, _ = ConfirmEmailToken.objects.get_or_create(user_id=user_id)
+    #token, _ = ConfirmEmailToken.objects.get_or_create(user_id=user_id)
     # subject, from_email, to = f"Password Reset Token for {token.user.email}", settings.EMAIL_HOST_USER, token.user.email
     # text_content = token.key
     # #html_content = '<p>This is an <strong>important</strong> message.</p>'
     # msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     # #msg.attach_alternative(html_content, "text/html")
     # msg.send(fail_silently=False)
-    return token
+    i = user_id+100
+    return i
 
 @app.task
 def d():
