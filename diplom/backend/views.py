851c438d7aab525b02414c9b07c32ab94e661e0e
SergeyRO <sergey_r.o@mail.ru>
2023-04-15 14:50:11 +0300

    second

--- diplom/backend/views.py
+++ diplom/backend/views.py
@@ -27,7 +27,8 @@ from backend.serializers import UserSerializer, CategorySerializer, ShopSerializ
 # from backend.signals import send_email_token, \
 #     send_email_reg, send_email_order, send_email_order_adm, \
 #     send_email_order_contact, do_import
-from netology_pd_diplom.celery import app, shared_task
+from netology_pd_diplom.celery import app
+from celery import shared_task
 
 @app.task
 def send_message(title, message, email):
