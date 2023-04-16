d86096fb75edef2011ef32bbede15a68da92f7ba
SergeyRO <sergey_r.o@mail.ru>
2023-04-16 07:01:17 +0300

    second

--- tests/backend/test_backend_api.py
+++ tests/backend/test_backend_api.py
@@ -60,11 +60,14 @@ from model_bakery import baker
 @pytest.mark.django_db
 def test_create_user(request):
     count_users_start = User.objects.count()
-    response = requests.post('http://127.0.0.1:1333/api/v1/user/register', json={"first_name": "Serge1",
+    print(count_users_start)
+    print(ConfirmEmailToken.objects.get())
+    print(User.objects.get())
+    response = requests.post('http://127.0.0.1:1333/api/v1/user/register', data={"first_name": "Serge1",
                                                                                  "last_name": "Rogch1",
                                                           "email": "sergey_r.o@mail.ru", "password": "password",
                                                           "company": "nelt11", "position": 1, "type": "shop",
-                                                          "username": "gggg"})
+                                                          "username": "gggg"}, format='json')
     assert response.status_code == 200
     assert User.objects.count() == count_users_start + 1
     request.config.cache.set('token_key', response.key)
