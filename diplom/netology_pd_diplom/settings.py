0d1427b8024427486d328c8a0b9d690d1fcd6ac4
SergeyRO <sergey_r.o@mail.ru>
2023-05-01 14:23:50 +0300

    second

--- diplom/netology_pd_diplom/settings.py
+++ diplom/netology_pd_diplom/settings.py
@@ -77,9 +77,9 @@ AUTHENTICATION_BACKENDS = [
 # 'secret': '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',
 
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:1333']
-APPEND_SLASH = False
-LOGIN_REDIRECT_URL = "/"
+# CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:1333']
+# APPEND_SLASH = False
+# LOGIN_REDIRECT_URL = "/"
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
     'vk': {
