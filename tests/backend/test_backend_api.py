3a2098a8b8c362ebaf699c2f29354cd47d1ab13f
SergeyRO <sergey_r.o@mail.ru>
2023-05-11 22:39:31 +0300

    f

--- tests/backend/test_backend_api.py
+++ tests/backend/test_backend_api.py
@@ -2,7 +2,7 @@ import random
 import requests
 import pytest
 from rest_framework.test import APIClient
-from backend.models import User, Shop, Category, \
+from diplom.backend.models import User, Shop, Category, \
     Product, ProductInfo, Parameter, ProductParameter, \
     Contact, Order, OrderItem, ConfirmEmailToken
 from model_bakery import baker
