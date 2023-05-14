e436dec3bebf2aedc40e215a8bc1fc6c6a5f0709
SergeyRO <sergey_r.o@mail.ru>
2023-05-14 12:20:17 +0300

    f

--- tests/backend/test_backend_api.py
+++ tests/backend/test_backend_api.py
@@ -28,11 +28,11 @@ def test_user_admin():
 @pytest.mark.django_db
 def test_create_user(client, request):
     count_users_start = User.objects.count()
-    
+
     User.objects.create(first_name="Serge11", last_name="Rogch11", email="1sergey_r.o@mail.ru", password="password1",
                         company="nelt111", position=2, type="admin", username="gggg", is_superuser=True, is_active=True)
 
-    print(User.objects.count())
+    print(f"cccc=====> {User.objects.count()}")
     admin = User.objects.get(email='1sergey_r.o@mail.ru')
 
     print(f"COUNT ===>  {count_users_start}")
